aba676a7cfcba8e1167c9fed6c87c27c36ee0675
Vincent Palmer <shift@someone.section.me>
2025-06-22 15:53:58 +0200

    chore(users/shift): Included my sway config from my other flake.
    Signed-off-by: Vincent Palmer <shift@someone.section.me>

--- users/shift/home.nix
+++ users/shift/home.nix
@@ -1,13 +1,9 @@
 { pkgs, ... }:
 {
   imports = [
-    ## Modularize your home.nix by moving statements into other files
+     ./sway.nix
   ];
   home.stateVersion = "25.05"; # Don't change this. This will not upgrade your home-manager.
   programs.home-manager.enable = true;
   programs.neovim.enable = true;
-  home.packages = with pkgs; [
-    # Common packages
-    hello
-  ];
 }
