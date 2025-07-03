397ade05a6918fda3131713f97c29481317a27e4
Vincent Palmer <shift@someone.section.me>
2025-07-03 16:39:02 +0200

    chore(squeals): Added their wallpaper for stylix to kick in.
    Signed-off-by: Vincent Palmer <shift@someone.section.me>

--- users/squeals/home.nix
+++ users/squeals/home.nix
@@ -3,12 +3,23 @@
   imports = [
     ## Modularize your home.nix by moving statements into other files
   ];
-
+  stylix = {
+    enable = true;
+    autoEnable = true;
+    image = ../../assets/squeals/wallpaper.png;
+    targets = {
+      firefox.profileNames = [ "default" ];
+    };
+  };
   home.stateVersion = "25.05"; # Don't change this. This will not upgrade your home-manager.
   programs.home-manager.enable = true;
 
+  #### You can edit this below here
+
+  programs.firefox.enable = true;
+
   home.packages = with pkgs; [
-    # Common packages
-    hello
+    reaper # Music editing software
+    davinci-resolve # Video editing software
   ];
 }
